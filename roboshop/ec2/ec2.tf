eda524036b02f70479f8add559f70b2924879e0f
cloudecoder <sandeepnet45@gmail.com>
2021-06-21 18:57:37 +0530

    terraform tf

--- roboshop/ec2/ec2.tf
+++ roboshop/ec2/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_spot_instance_request" "cheap_worker" {
+resource "aws_spot_instance_request" "spot_instance" {
   ami           = "ami-059e6ca6474628ef0"
   spot_price    = "0.0031"
   instance_type = "t3.micro"
