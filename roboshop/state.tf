09b4110c33a83689cdcc43ec60eda39f5777c755
cloudecoder <sandeepnet45@gmail.com>
2021-07-03 18:25:06 +0530

    terraform

--- roboshop/state.tf
+++ roboshop/state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket           = "storetf"
-    key              = "roboshop/tfstate"
+    key              = "terraform/tfstate"
     region           = "us-east-1"
     dynamodb_table   = "terraform"
   }
