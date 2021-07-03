e4f0731833ec9e2c4ee256373db9ab147a002d5a
cloudecoder <sandeepnet45@gmail.com>
2021-07-03 18:41:04 +0530

    terraform

--- roboshop/ec2/ec2.tf
+++ roboshop/ec2/ec2.tf
@@ -20,18 +20,18 @@ resource "aws_ec2_tag" "spot" {
   depends_on                  = [time_sleep.waiting]
   count                       = length(var.COMPONENTS)
   key                         = "name"
-  resource_id                 = element(aws_spot_instance_request.launch.*.id ,count.index)
+  resource_id                 = element(aws_spot_instance_request.launch.*.spot_instance_id,count.index)
   value                       = element(var.COMPONENTS,count.index)
 }
 
 resource "aws_route53_record" "dns" {
   depends_on                  = [aws_ec2_tag.spot]
   count                       = length(var.COMPONENTS)
-  name                        = "${element(var.COMPONENTS,count.index)}.roboshop.internal"
+  name                        = "${element(var.COMPONENTS, count.index)}.roboshop.internal"
   type                        = "A"
   zone_id                     = "Z048532427Z8A2VSNE7P3"
   ttl                         = "300"
-  records                     = [element(aws_spot_instance_request.launch.*.private_ip,count.index)]
+  records                     = [element(aws_spot_instance_request.launch.*.private_ip, count.index)]
 }
 
 
