33fdde3e2cf13832d23a1a0f01f05cdbd697f6dd
cloudecoder <sandeepnet45@gmail.com>
2021-07-03 18:58:31 +0530

    terraform

--- roboshop/ec2/ec2.tf
+++ roboshop/ec2/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_spot_instance_request" "launch" {
   ami                         = "ami-059e6ca6474628ef0"
   spot_price                  = "0.0031"
   instance_type               = "t2.micro"
-
+  vpc_security_group_ids      = ["sg-078ae966242083129"]
 
   tags = {
     Name                      = element(var.COMPONENTS,count.index )
@@ -13,7 +13,7 @@ variable "COMPONENTS" {}
 
 resource "time_sleep" "waiting" {
   depends_on                  = [aws_spot_instance_request.launch]
-  create_duration             = "30s"
+  create_duration             = "120s"
 }
 
 resource "aws_ec2_tag" "spot" {
