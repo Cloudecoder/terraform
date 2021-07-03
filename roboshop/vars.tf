b6dc1940ebfe6edcc531bfdb9a4aeec8b8da4a91
cloudecoder <sandeepnet45@gmail.com>
2021-07-03 17:23:59 +0530

    terraform

--- roboshop/vars.tf
+++ roboshop/vars.tf
@@ -2,10 +2,4 @@ variable "COMPONENTS" {
   default = ["frontend","mongodb","catalogue","mysql","redis","cart","user","rabbitmq","payment","shipping"]
 }
 
-output "number_of_instance" {
-  value = length(var.COMPONENTS)
-}
 
-output "instance" {
-  value = element(var.COMPONENTS)
-}
