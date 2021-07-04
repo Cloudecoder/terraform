1693f2b505f4a74bd9c3235c98fa65f9153eca6d
cloudecoder <sandeepnet45@gmail.com>
2021-07-04 16:03:31 +0530

    terraform

--- roboshop/ansible-apply/ansible.tf
+++ roboshop/ansible-apply/ansible.tf
@@ -1,25 +1,40 @@
-//resource "null_resource" "ansible" {
-//  triggers = {
-//    trig_when_chng       = timestamp()
-//  }
-//  count                  = length(var.COMPONENTS)
-//
-//   provisioner "remote-exec" {
-//    connection {
-//      host               = "${element(var.COMPONENTS, count.index)}.roboshop.internal"
-//      user               = "centos"
-//      password           = "DevOps321"
-//    }
-//    inline = [
-//     "sudo yum install ansible -y",
-//      "sudo yum remove ansible -y",
-//      "sudo rm -rf /usr/lib/python2.7/site-packages/ansible*",
-//      "sudo pip install ansible=3.4.0",
-//      "ansible pull -i localhost, -U *git repo roboshop.pull.yml -e COMPONENT=${element(var.COMPONENTS, count.index )}"
-//
-//    ]
-//  }
-//}
-//
-//
-//variable "COMPONENTS" {}
+resource "null_resource" "ansible" {
+  triggers = {
+    abc = timestamp()
+  }
+  count                = length(var.COMPONENTS)
+  provisioner "remote-exec" {
+    connection {
+      host = " ${element(var.COMPONENTS,count.index)}.roboshop.internal"
+      user = "centos"
+      password = "DevOps321"
+    }
+    inline = [
+      "sudo yum install ansible -y",
+      "sudo yum remove ansible -y",
+      "sudo rm -rf /usr/lib/python2.7/site-packages/ansible*",
+      "sudo pip install ansible=3.4.0",
+      "ansible-pull -i localhost, -U https://github.com/Cloudecoder/ansible.git ansible-pull.yml -e COMPONENT=${element(var.COMPONENTS,count.index)}"
+    ]
+
+  }
+}
+
+
+
+variable "COMPONENTS" {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
