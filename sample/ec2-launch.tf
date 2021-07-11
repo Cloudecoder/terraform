d71d846b5d59b2affcac3461d23ad3c41f89a41d
cloudecoder <sandeepnet45@gmail.com>
2021-07-11 15:58:42 +0530

    ans

--- sample/ec2-launch.tf
+++ sample/ec2-launch.tf
@@ -1,6 +1,10 @@
-resource "aws_instance" "launch" {
+resource "aws_spot_instance_request" "launch" {
   count            = 2
   ami              = "ami-0dc2d3e4c0f9ebd18"
   instance_type    = "t2.micro"
 
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
